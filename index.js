95727e824aa43567dea4c2777ea9f7d37bc2a6e8
Peter <plasne@iname.com>
2017-10-11 11:38:54 -0400

    fixed arm

--- index.js
+++ index.js
@@ -18,7 +18,25 @@ const workspaceId = config.get("workspaceId");
 
 cmd
     .version("0.2.0")
-    .option("--api <value>", "Specify the API to use between: legacy, direct, azure.");
+    .option("--api <value>", "Specify the API to use between: legacy, direct, azure.")
+    .option("--docs", "Show all documentation links.")
+    .option("-r, --resource-group <value>", "Specify a Resource Group other than the one in the settings file.")
+    .option("-w, --workspace <value>", "Specify a Workspace Name other than the one in the settings file.")
+    .option("-i, --workspace-id <value>", "Specify a Workspace ID other than the one in the settings file.");
+
+const use_resourceGroup = cmd.resourceGroup || resourceGroup;
+const use_workspace = cmd.workspace || workspace;
+const use_workspaceId = cmd.workspaceId || workspaceId;
+
+cmd
+    .command("docs")
+    .description("Show all documentation links.")
+    .action(_ => {
+        console.log("New Query API:")
+        console.log("  https://docs.loganalytics.io/");
+        console.log("  https://dev.loganalytics.io/");
+        console.log("  https://dev.loganalytics.io/documentation/1-Tutorials/ARM-API - you must follow these instructions to configure application permissions.")
+    });
 
 cmd
     .command("consent")
@@ -34,14 +52,12 @@ cmd
 cmd
     .command("info")
     .description("Get the info regarding the workspace.")
-    .option("-w, --workspace <value>", "Specify a workspace other than the one in the settings file")
-    .action(options => {
-        const effective_workspace = options.workspace || workspace;
+    .action(_ => {
         const context = new adal.AuthenticationContext(authority + directory);
         context.acquireTokenWithClientCredentials("https://management.azure.com/", clientId, clientSecret, (error, tokenResponse) => {
             if (!error) {
                 request.get({
-                    uri: `https://management.azure.com/subscriptions/${subscription}/resourcegroups/${resourceGroup}/providers/Microsoft.OperationalInsights/workspaces/${effective_workspace}?api-version=2015-03-20`,
+                    uri: `https://management.azure.com/subscriptions/${subscription}/resourcegroups/${use_resourceGroup}/providers/Microsoft.OperationalInsights/workspaces/${use_workspace}?api-version=2015-03-20`,
                     headers: {
                         Authorization: `Bearer ${tokenResponse.accessToken}`
                     },
@@ -49,8 +65,10 @@ cmd
                 }, (error, response, body) => {
                     if (!error && response.statusCode == 200) {
                         console.log(body);
+                    } else if (error) {
+                        console.error(`error(401): ${error}`);
                     } else {
-                        if (error) { console.error("error(401): " + error) } else { console.error("error(402)"); console.log(body); };
+                        console.error(`error(402) ${response.statusCode}: ${response.statusMessage}`);
                     }
                 });
             } else {
@@ -72,22 +90,24 @@ cmd
                 context1.acquireTokenWithClientCredentials("https://management.azure.com/", clientId, clientSecret, (error, tokenResponse) => {
                     if (!error) {
                         request.post({
-                            uri: `https://management.azure.com/subscriptions/${subscription}/resourceGroups/${resourceGroup}/providers/Microsoft.OperationalInsights/workspaces/${workspace}/search?api-version=2015-03-20`,
+                            uri: `https://management.azure.com/subscriptions/${subscription}/resourceGroups/${use_resourceGroup}/providers/Microsoft.OperationalInsights/workspaces/${use_workspace}/search?api-version=2015-03-20`,
                             headers: {
                                 Authorization: `Bearer ${tokenResponse.accessToken}`
                             },
                             body: {
-                                top: 100,
-                                query: "Type=Event or Type=Syslog",
-                                start: "2017-04-17T00:00:00Z",
-                                end: "2017-04-17T23:59:59Z"
+                                top: 5,
+                                query: "*",
+                                start: new Date(new Date() - 24 * 60 * 60 * 1000).toISOString(),
+                                end: new Date().toISOString()
                             },
                             json: true
                         }, (error, response, body) => {
                             if (!error && response.statusCode == 200) {
                                 console.log(body);
+                            } else if (error) {
+                                console.error(`error(101): ${error}`);
                             } else {
-                                if (error) { console.error("error(101): " + error) } else { console.error("error(102)"); console.log(body); };
+                                console.error(`error(102) ${response.statusCode}: ${response.statusMessage}`);
                             }
                         });
                     } else {
@@ -103,20 +123,23 @@ cmd
                 context2.acquireTokenWithClientCredentials("https://api.loganalytics.io", clientId, clientSecret, (error, tokenResponse) => {
                     if (!error) {
                         request.post({
-                            uri: `https://api.loganalytics.io/v1/workspaces/${workspaceId}/query`,
+                            uri: `https://api.loganalytics.io/v1/workspaces/${use_workspaceId}/query`,
                             headers: {
                                 Authorization: `Bearer ${tokenResponse.accessToken}`
                             },
                             body: {
-                                query: "Perf",
-                                timespan: "PT12H"
+                                query: "search * | limit 5",
+                                timespan: "PT24H"
                             },
                             json: true
                         }, (error, response, body) => {
                             if (!error && response.statusCode == 200) {
                                 console.log(body);
+                            } else if (error) {
+                                console.error(`error(201): ${error}`);
                             } else {
-                                if (error) { console.error("error(201): " + error) } else { console.error("error(202)"); console.log(body); };
+                                console.error(`error(202) ${response.statusCode}: ${response.statusMessage}`);
+                                console.log("NOTE: This error is expected since the Direct API does not currently support application permissions, only delegate permissions.");
                             }
                         });
                     } else {
@@ -125,31 +148,33 @@ cmd
                 });
                 break;
 
-            case "azure":
+            case "arm":
 
-                // query using the new Azure API
+                // query using the new ARM API
                 const context3 = new adal.AuthenticationContext(authority + directory);
                 context3.acquireTokenWithClientCredentials("https://management.azure.com/", clientId, clientSecret, (error, tokenResponse) => {
                     if (!error) {
                         request.post({
-                            uri: `https://management.azure.com/subscriptions/${subscription}/resourceGroups/${resourceGroup}/providers/Microsoft.OperationalInsights/workspaces/${workspaceId}/query?api-version=2017-04-26-preview`,
+                            uri: `https://management.azure.com/subscriptions/${subscription}/resourceGroups/${use_resourceGroup}/providers/Microsoft.OperationalInsights/workspaces/${use_workspace}/api/query?api-version=2017-01-01-preview`,
                             headers: {
                                 Authorization: `Bearer ${tokenResponse.accessToken}`
                             },
                             body: {
-                                query: "union Event, Syslog",
-                                timespan: "PT12H"
+                                query: "search * | limit 5",
+                                timespan: "PT24H"
                             },
                             json: true
                         }, (error, response, body) => {
                             if (!error && response.statusCode == 200) {
                                 console.log(body);
+                            } else if (error) {
+                                console.error(`error(301): ${error}`);
                             } else {
-                                if (error) { console.error("error(301): " + error) } else { console.error("error(302)"); console.log(body); };
+                                console.error(`error(302) ${response.statusCode}: ${response.statusMessage}`);
                             }
                         });
                     } else {
-                        console.error("error(300): " + error);
+                        console.error(`error(300): ${error}`);
                     }
                 });
                 break;
